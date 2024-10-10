39d8884fb0eddac0b55ec9eb848f55aa9770414d
Angelo Franciamore <angelo99franciamore@gmail.com>
2024-10-10 10:24:53 +0200

    first commit

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+// import 'package:geolocator/geolocator.dart';
+import '../services/location.dart';
+
+class LoadingScreen extends StatefulWidget {
+  const LoadingScreen({super.key});
+
+  @override
+  State<LoadingScreen> createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
+  void getLocation() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+
+    print(location.latitude);
+    print(location.longitude);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          style: ButtonStyle(
+              backgroundColor:
+                  WidgetStateProperty.all<Color>(Colors.blue.shade50)),
+          onPressed: () {
+            getLocation();
+          },
+          child: Text(
+            'Get Location',
+            style: TextStyle(
+              color: Colors.blue.shade700,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/services/location.dart
@@ -0,0 +1,26 @@
+import 'package:geolocator/geolocator.dart';
+import 'package:flutter/material.dart';
+
+class Location {
+  double latitude = 0;
+  double longitude = 0;
+
+  Future<void> getCurrentLocation() async {
+    try {
+      // ignore: prefer_const_constructors
+      final LocationSettings locationSettings = LocationSettings(
+        accuracy: LocationAccuracy.low,
+        distanceFilter: 100,
+      );
+      LocationPermission permission = await Geolocator.requestPermission();
+
+      Position position = await Geolocator.getCurrentPosition(
+          locationSettings: locationSettings);
+
+      latitude = position.latitude;
+      longitude = position.longitude;
+    } catch (e) {
+      print(e);
+    }
+  }
+}

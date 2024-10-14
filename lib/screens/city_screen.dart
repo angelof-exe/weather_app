93c3c05e4d1e0b9223e23a56e23eceb3999cd3d3
Angelo Franciamore <angelo99franciamore@gmail.com>
2024-10-14 12:06:17 +0200

    refact: added InputField

--- lib/screens/city_screen.dart
+++ lib/screens/city_screen.dart
@@ -8,6 +8,8 @@ class CityScreen extends StatefulWidget {
 }
 
 class _CityScreenState extends State<CityScreen> {
+  late String cityName;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,8 +38,20 @@ class _CityScreenState extends State<CityScreen> {
                 padding: const EdgeInsets.all(20.0),
                 child: null,
               ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 28.0, vertical: 12),
+                child: TextField(
+                  style: TextStyle(color: Colors.black),
+                  decoration: kTextFieldInputDecoration,
+                  onChanged: (value) {
+                    cityName = value;
+                  },
+                ),
+              ),
               TextButton(
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.pop(context, cityName);
+                },
                 child: const Text(
                   'Get Weather',
                   style: kButtonTextStyle,
